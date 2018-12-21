8947f49f1d0144c8abd144bba19dfd64351342b6
Eric Traut <erictr@microsoft.com>
2018-12-20 22:37:30 -0800

    Added custom Bonsai color theme.

--- lib/rouge/themes/base16.rb
+++ lib/rouge/themes/base16.rb
@@ -125,6 +125,28 @@ module Rouge
         palette base0E: "#ae81ff"
         palette base0F: "#cc6633"
       end
+
+      class Bonsai < Base16
+        name 'base16.bonsai'
+        dark!
+
+        palette base00: "#272822"
+        palette base01: "#383830"
+        palette base02: "#49483e"
+        palette base03: "#d59e52"
+        palette base04: "#a59f85"
+        palette base05: "#ffffff"
+        palette base06: "#f5f4f1"
+        palette base07: "#f9f8f5"
+        palette base08: "#f92672"
+        palette base09: "#fd971f"
+        palette base0A: "#f4bf75"
+        palette base0B: "#ffffff"
+        palette base0C: "#a1efe4"
+        palette base0D: "#66d9ef"
+        palette base0E: "#51beff"
+        palette base0F: "#cc6633"
+      end
     end
   end
 end
